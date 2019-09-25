891d391238bfa81e7387ed75360d9dbcd45f208c
ArturoOrtiz1 <54646804+ArturoOrtiz1@users.noreply.github.com>
2019-09-24 22:07:49 -0400

    Add files via upload

---
+++ Lab 5.py
@@ -0,0 +1,16 @@
+def main():
+    print("This program turns numerical grade into letter grade")
+    n=int(input("Enter numerical grade:"))
+    if n >90: 
+        print('A')
+    elif n > 80: 
+        print ('B')
+    elif n >70: 
+        print ('C')
+    elif n >60: 
+        print ('D')
+    elif n >0: 
+        print ('F')
+
+main()
+    
